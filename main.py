292bd347720122bab2e6e8af84ad994c255c08fc
amandeep55 <78647639+amandeep55@users.noreply.github.com>
2021-11-24 07:19:09 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -1,120 +1,183 @@
-from subprocess import call
-try:
-    from Tkinter import *
-except ImportError:
-    from tkinter import *
+import csv
+file1 = open('login.csv')
+csvreader = csv.reader(file1)
+header = []
+header = next(csvreader)
+rows=[]
+for row in csvreader:
+    rows.append(row)
+import  os
+def Advisior():
+  import csv
+  import os
+  book = open('bookings.csv')
+  csvreader1 = csv.reader(book)
+  header = []
+  header = next(csvreader1)
+  booking=[]
+  for row in csvreader1:
+          booking.append(row)
+  av = open('aval.csv')
+  csvreader2 = csv.reader(av)
+  header = []
+  header = next(csvreader2)
+  ava=[]
+  for row in csvreader2:
+          ava.append(row)
+  print("<------Login successfull as Advisior--------->")
+  print("1.check boookings  :: ")
+  print("2.Booking approvel  :: ")
+  print("3.Check avaliablety :: ")
+  a=int(input())
+  if a==1:
+    print("Location      TYPE       Date        UserID")
+    for i in range(len(booking)):
+      print(booking[i][0],"    ",booking[i][1],"   ",booking[i][2],"    ",booking[i][3])
+    Advisior()
+  elif a==2:
+    print("SNo    ","Location      TYPE       Date        UserID")
+    for i in range(len(booking)):
+      print(i+1,"    ", booking[i][0],"    ",booking[i][1],"   ",booking[i][2],"    ",booking[i][3])
+    a=(int(input("select booking ::")))-1
+    for i in range(len(ava)):
+      if booking[a][0]==ava[i][0] and ava[i][2]==str(0) and ava[i][3]==booking[a][1].lower():
+        print("Booking successfull")
+        ava[i][2]=1
 
-try:
-    import ttk
-
-    py3 = False
-except ImportError:
-    import tkinter.ttk as ttk
-
-    py3 = True\
-
-class SOLENT_CAMPERS:
-    def __init__(self):
-        root = Tk()
-        _bgcolor = '#d9d9d9'
-        _fgcolor = '#000000'
-        _compcolor = '#ffffff'
-        _ana1color = '#ffffff'
-        _ana2color = '#ffffff'
-        font14 = "-family {Segoe UI} -size 15 -weight bold -slant " \
-                 "roman -underline 0 -overstrike 0"
-        font16 = "-family {Swis721 BlkCn BT} -size 40 -weight bold " \
-                 "-slant roman -underline 0 -overstrike 0"
-        font9 = "-family {Segoe UI} -size 9 -weight normal -slant " \
-                "roman -underline 0 -overstrike 0"
-
-        root.geometry("963x749+540+110")
-        root.title("Solent Campers")
-        root.configure(background="#d9d9d9")
-        root.configure(highlightbackground="#d9d9d9")
-        root.configure(highlightcolor="black")
-
-        self.menubar = Menu(root, font=font9, bg=_bgcolor, fg=_fgcolor)
-        root.configure(menu=self.menubar)
-
-        self.Frame1 = Frame(root)
-        self.Frame1.place(relx=0.02, rely=0.03, relheight=0.94, relwidth=0.96)
-        self.Frame1.configure(relief=GROOVE)
-        self.Frame1.configure(borderwidth="2")
-        self.Frame1.configure(relief=GROOVE)
-        self.Frame1.configure(background="#d9d9d9")
-        self.Frame1.configure(highlightbackground="#d9d9d9")
-        self.Frame1.configure(highlightcolor="black")
-        self.Frame1.configure(width=925)
-
-        self.Message6 = Message(self.Frame1)
-        self.Message6.place(relx=0.09, rely=0.01, relheight=0.15, relwidth=0.86)
-        self.Message6.configure(background="#d9d9d9")
-        self.Message6.configure(font=font16)
-        self.Message6.configure(foreground="#000000")
-        self.Message6.configure(highlightbackground="#d9d9d9")
-        self.Message6.configure(highlightcolor="black")
-        self.Message6.configure(text="Select User")
-        self.Message6.configure(width=791)
-
-        self.Button2 = Button(self.Frame1)
-        self.Button2.place(relx=0.18, rely=0.20, height=103, width=566)
-        self.Button2.configure(activebackground="#d9d9d9")
-        self.Button2.configure(activeforeground="#000000")
-        self.Button2.configure(background="#d9d9d9")
-        self.Button2.configure(disabledforeground="#bfbfbf")
-        self.Button2.configure(font=font14)
-        self.Button2.configure(foreground="#000000")
-        self.Button2.configure(highlightbackground="#d9d9d9")
-        self.Button2.configure(highlightcolor="black")
-        self.Button2.configure(pady="0")
-        self.Button2.configure(text="Customer")
-        self.Button2.configure(width=566)
-
-        self.Button3 = Button(self.Frame1)
-        self.Button3.place(relx=0.18, rely=0.39, height=93, width=566)
-        self.Button3.configure(activebackground="#d9d9d9")
-        self.Button3.configure(activeforeground="#000000")
-        self.Button3.configure(background="#d9d9d9")
-        self.Button3.configure(disabledforeground="#bfbfbf")
-        self.Button3.configure(font=font14)
-        self.Button3.configure(foreground="#000000")
-        self.Button3.configure(highlightbackground="#d9d9d9")
-        self.Button3.configure(highlightcolor="black")
-        self.Button3.configure(pady="0")
-        self.Button3.configure(text="Advisor")
-        self.Button3.configure(width=566)
-
-        self.Button4 = Button(self.Frame1)
-        self.Button4.place(relx=0.18, rely=0.56, height=93, width=566)
-        self.Button4.configure(activebackground="#d9d9d9")
-        self.Button4.configure(activeforeground="#000000")
-        self.Button4.configure(background="#d9d9d9")
-        self.Button4.configure(disabledforeground="#bfbfbf")
-        self.Button4.configure(font=font14)
-        self.Button4.configure(foreground="#000000")
-        self.Button4.configure(highlightbackground="#d9d9d9")
-        self.Button4.configure(highlightcolor="black")
-        self.Button4.configure(pady="0")
-        self.Button4.configure(text="Admin")
-        self.Button4.configure(width=566)
-
-        self.Button6 = Button(self.Frame1)
-        self.Button6.place(relx=0.38, rely=0.81, height=50, width=200)
-        self.Button6.configure(activebackground="#d9d9d9")
-        self.Button6.configure(activeforeground="#000000")
-        self.Button6.configure(background="#d9d9d9")
-        self.Button6.configure(disabledforeground="#bfbfbf")
-        self.Button6.configure(font=font14)
-        self.Button6.configure(foreground="#000000")
-        self.Button6.configure(highlightbackground="#d9d9d9")
-        self.Button6.configure(highlightcolor="black")
-        self.Button6.configure(pady="0")
-        self.Button6.configure(text="Exit")
-        self.Button6.configure(width=200)
-        self.Button6.configure(command=quit)
-        root.mainloop()
+        Advisior()
+  elif a==3:
+    print("Location     cab number     condition")
+    for i in range(len(ava)):
+      print(ava[i][0],"     ",ava[i][1],"     ",ava[i][2])
+    Advisior()
+  else:
+    print("Wrong input")
+  file="aval.csv"
+  if(os.path.exists(file) and os.path.isfile(file)):
+    os.remove(file)
+  fields=["Location","cab number","condition","type"]
+  with open("aval.csv", 'w') as csvfile: 
+    csvwriter = csv.writer(csvfile) 
+    csvwriter.writerow(fields)
+    csvwriter.writerows(ava)
+  file="bookings.csv"
+  if(os.path.exists(file) and os.path.isfile(file)):
+    os.remove(file)
+  fields=["Locations","Type","Date","UserID","Status"]
+  with open("bookings.csv", 'w') as csvfile: 
+    csvwriter = csv.writer(csvfile) 
+    csvwriter.writerow(fields)
+    csvwriter.writerows(ava)
+def admin():
+  import csv,os
+  av = open('aval.csv')
+  csvreader2 = csv.reader(av)
+  header = []
+  header = next(csvreader2)
+  ava=[]
+  for row in csvreader2:
+      ava.append(row)
+  print("<----------------login successfull as a admin------------>")
+  print("1. Check summary of the van::")
+  print("2. add new van::")
+  a=int(input())
+  if a==1:
+    print("Location     cab number     condition    type")
+    for i in range(len(ava)):
+      print(ava[i][0],"     ",ava[i][1],"     ",ava[i][2],"     ",ava[i][3])
+    admin()
+  elif a==2:
+    l=input("Enter the locations ::")
+    n=input("Enter the cab number::")
+    c=input("enter the condition(in 1 or 0)::")
+    t=input("Enter the type ::")
+    k=[]
+    k.append(l)
+    k.append(n)
+    k.append(c)
+    k.append(t)
+    ava.append(k)
+    file="aval.csv"
+    if(os.path.exists(file) and os.path.isfile(file)):
+      os.remove(file)
+    fields=["Location","cab number","condition","type"]
+    with open("aval.csv", 'w') as csvfile: 
+      csvwriter = csv.writer(csvfile) 
+      csvwriter.writerow(fields)
+      csvwriter.writerows(ava)
+    admin()
+  else:
+    print("Wrog input logout successfully")
+def customer(e):
+  print("<----Login successfull as a Customer--->")
+  print("select your choice::")
+  print("1.Request to advisior to Book the van")
+  print("2.See the summary")
+  print("for logout press any key::")
+  a=int(input())
+  k=[]
+  r=[]
+  l=0
+  t=0
+  ti=0
+  vn=0
+  s="pending"
+  if a==1:
+    l=str(input("ENter the location:"))
+    t=str(input("Enter the van size small/medium/large::"))
+    ti=str(input("enter tha date::"))
+    
+    r.append([l,t,ti,e,s])
+    print("Request added successfuly",r)
+    customer(e)
+  elif a==2:
+    print("location name:",l)
+    print("van number",vn)
+    print("van type:",t)
+    print("data:",ti)
+    print("Status:",s)
+    customer(e)
+  else:
+    print("Thanks for services")
+  with open("bookings.csv", 'w') as csvfile: 
+    csvwriter = csv.writer(csvfile) 
+    csvwriter.writerows(r)
 
 
-if __name__ == '__main__':
-    GUEST = SOLENT_CAMPERS()
+
+def main():
+  filename = "bookings.csv"
+  fields=["Locations","Type","Date","UserID","Status"]
+  with open("bookings.csv", 'w') as csvfile: 
+    # creating a csv writer object 
+    csvwriter = csv.writer(csvfile) 
+        
+    # writing the fields 
+    csvwriter.writerow(fields) 
+  print("<----------Please login --------->")
+  u=int(input("Enter the type of user 1 (customer) 2(Advisior) 3 (Admin)::-"))
+  if u==1 or u==2 or u==3:
+    pass
+  else:
+    print("wrong credintial login again")
+    main()
+  e=input("Enter the UserID::-")
+  p=input("password::-")
+  for i in range(len(rows)):
+    if rows[i][0]==e:
+
+      a=i
+      print(a)
+      if rows[a][0]==e and rows[a][1]==p and rows[a][2]==str(u):
+        if rows[a][2]=="1":
+          customer(e)
+        elif rows[a][2]=="2":
+          Advisior()
+        elif rows[a][2]=="3":
+          admin()
+      else:
+        print("wrong credintial login again")
+        main()
+    else:
+      pass
+main()
